2d6353d3630dc229e7dc760e748161b12424dc49
yamatatsu <the.kinnikuman@gmail.com>
2019-05-01 14:42:35 +0900

    hoge

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path')
-const webpack = require('webpack')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const { npm_package_slideKey } = process.env
+const { npm_package_slideKey } = process.env;
 
 module.exports = {
-  resolve: {
-    extensions: ['.json'],
-  },
-  plugins: [
-    new webpack.IgnorePlugin(/^fs$/),
-    new CopyPlugin([{ from: 'public' }]),
-    new PrerenderSPAPlugin({
-      // `dist` に置いてある index.html を SPA として読み込み
-      staticDir: path.join(__dirname, '../../dist'),
-      // SPA 内で `/${npm_package_slideKey}` にアクセスして
-      routes: [`/`],
-      // 'dist' に snapshot を置く。
-      // この時、routesで指定した `/${npm_package_slideKey}` に置くため、
-      // `dist/${npm_package_slideKey}/index.html` が上書きされる
-      // outputDir: path.join(__dirname, '../../dist'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-    ],
-  },
-}
+	resolve: {
+		extensions: ['.json']
+	},
+	plugins: [
+		new webpack.IgnorePlugin(/^fs$/),
+		new CopyPlugin([{ from: 'public' }]),
+		new PrerenderSPAPlugin({
+			// `dist` に置いてある index.html を SPA として読み込み
+			staticDir: path.join(__dirname, '../../dist'),
+			// SPA 内で `/${npm_package_slideKey}` にアクセスして
+			routes: [`/`]
+			// 'dist' に snapshot を置く。
+			// この時、routesで指定した `/${npm_package_slideKey}` に置くため、
+			// `dist/${npm_package_slideKey}/index.html` が上書きされる
+			// outputDir: path.join(__dirname, '../../dist'),
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ['file-loader']
+			}
+		]
+	}
+};
