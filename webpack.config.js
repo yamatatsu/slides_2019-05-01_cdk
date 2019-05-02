19a91367a2e39302c1c6b94a86305687209f0282
yamatatsu <the.kinnikuman@gmail.com>
2019-05-02 15:40:08 +0900

    hoge

--- webpack.config.js
+++ webpack.config.js
@@ -13,14 +13,8 @@ module.exports = {
 		new webpack.IgnorePlugin(/^fs$/),
 		new CopyPlugin([{ from: 'public' }]),
 		new PrerenderSPAPlugin({
-			// `dist` に置いてある index.html を SPA として読み込み
-			staticDir: path.join(__dirname, '../../dist'),
-			// SPA 内で `/${npm_package_slideKey}` にアクセスして
-			routes: [`/`]
-			// 'dist' に snapshot を置く。
-			// この時、routesで指定した `/${npm_package_slideKey}` に置くため、
-			// `dist/${npm_package_slideKey}/index.html` が上書きされる
-			// outputDir: path.join(__dirname, '../../dist'),
+			staticDir: path.join(__dirname, './dist'),
+			routes: ['/']
 		})
 	],
 	module: {
