b44ebecc64d4a7ec9cf09dcbd15d998f87ef9122
yamatatsu <the.kinnikuman@gmail.com>
2019-05-04 03:13:37 +0900

    redirect

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
-const path = require('path');
-const webpack = require('webpack');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const packageJson = require('./package.json')
 
-const { npm_package_slideKey } = process.env;
+const { npm_package_slideKey } = process.env
 
 module.exports = {
-	resolve: {
-		extensions: ['.json']
-	},
-	plugins: [
-		new webpack.IgnorePlugin(/^fs$/),
-		new CopyPlugin([{ from: 'public' }]),
-		new PrerenderSPAPlugin({
-			staticDir: path.join(__dirname, './dist'),
-			routes: ['/']
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: ['file-loader']
-			}
-		]
-	}
-};
+  resolve: {
+    extensions: ['.json'],
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^fs$/),
+    new CopyPlugin([{ from: 'public' }]),
+    new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, './dist'),
+      routes: [`/${packageJson.slideKey}`],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
